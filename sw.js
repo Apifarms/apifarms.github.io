00cabe53fd70e3d3330bb5fb3a3809768828374f
AlexCLeduc <ledualex@gmail.com>
2018-01-12 19:46:52 -0500

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-06f7fc4a7046185b734c.js","248e465ee50b6b32b0332492b04103bc"],["/app-b0147941e305527b47e6.js","65bc95150dd7f89f4240bb3229639bcc"],["/app-b0ce4b9b78d0096d7fb9.js","0eabe92c47addb28bf3a4c0273354c0f"],["/app-bd1a0db4d993a454edeb.js","90dc54e10bcae2284a706f8e9379662a"],["/app-d2b37f1631099931fd20.js","a0979cfd3f0aa22d7c57473d9f7c8f03"],["/commons-5f49b2a7256bc28b6596.js","dc0b380a534d0553299d1d5838409923"],["/commons-dd6fd2af12ccc9301063.js","8287b2bb82630ceb57a49495573542d4"],["/index.html","1dcc017608b681cd59c6739398a49415"],["/offline-plugin-app-shell-fallback/index.html","12aed5e632bc200be4c9d90299009c73"]];
+var precacheConfig = [["/app-06f7fc4a7046185b734c.js","248e465ee50b6b32b0332492b04103bc"],["/app-b0147941e305527b47e6.js","65bc95150dd7f89f4240bb3229639bcc"],["/app-b0ce4b9b78d0096d7fb9.js","0eabe92c47addb28bf3a4c0273354c0f"],["/app-bd1a0db4d993a454edeb.js","90dc54e10bcae2284a706f8e9379662a"],["/app-d2b37f1631099931fd20.js","a0979cfd3f0aa22d7c57473d9f7c8f03"],["/commons-5f49b2a7256bc28b6596.js","dc0b380a534d0553299d1d5838409923"],["/commons-dd6fd2af12ccc9301063.js","8287b2bb82630ceb57a49495573542d4"],["/index.html","8998140fbf1fdc5a391088342ccfdf0f"],["/offline-plugin-app-shell-fallback/index.html","7b5ec87b714d40c3184f6fa410c6043c"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
